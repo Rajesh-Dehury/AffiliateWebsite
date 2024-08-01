df81411e37ab2ec7c52756969f117b701bdaefbe
Rajesh-Dehury <56267680+Rajesh-Dehury@users.noreply.github.com>
2024-08-01 12:37:39 +0530

    dark mode

--- resources/views/components/admin-top-nav.blade.php
+++ resources/views/components/admin-top-nav.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-white dark:text-gray-200 dark:bg-teal-700 dark:border-teal-700 border-b py-1 px-3">
+<div class="bg-white dark:text-gray-200 dark:bg-gray-900 dark:border-gray-700 border-b py-1 px-3">
     <div class="flex justify-between items-center mt-1">
         <div class="flex justify-between items-center">
             <div class="flex items-center mr-4 md:hidden">
@@ -14,14 +14,14 @@
         </div>
         <div class="flex justify-end items-center">
             <div>
-                <a wire:navigate href="#" type="button" class="inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-teal-900 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5">
+                <a wire:navigate href="{{route('admin.logout')}}" type="button" class="inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M5.636 5.636a9 9 0 1012.728 0M12 3v9" />
                     </svg>
                 </a>
             </div>
             <div>
-                <span x-on:click="darkMode = true" type="button" class="inline-block ml-3 text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-teal-900 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5">
+                <span x-on:click="darkMode = !darkMode" type="button" class="inline-block ml-3 text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5">
                     <svg x-show="darkMode" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
                     </svg>
