86b4c8646491617a9b7b571ddefbf1194bd7c7cb
Rajesh-Dehury <56267680+Rajesh-Dehury@users.noreply.github.com>
2024-08-01 23:54:16 +0530

    ui change

--- resources/views/components/home-top-nav.blade.php
+++ resources/views/components/home-top-nav.blade.php
@@ -1,17 +1,26 @@
-<nav x-data="{ isOpen: false }" class="relative bg-white shadow dark:bg-gray-800">
-    <div class="container px-6 py-3 mx-auto md:flex">
-        <div class="flex items-center px-5">
+<nav x-data="{ isOpen: false, searchBox: false }" class="relative bg-white shadow dark:bg-gray-800">
+    <div class="container px-0 lg:px-6 py-3 mx-auto md:flex">
+        <div class="flex justify-between items-center px-5">
             <div>
                 <a class="text-xl font-bold text-gray-800 transition-colors duration-300 transform dark:text-white hover:text-gray-700 dark:hover:text-gray-300" href="#">
                     <div class="flex items-center">
                         <img src="{{asset('logo.png')}}" alt="" class="h-14 w-14 rounded-full">
-                        <p class="ml-0">DealsDay</p>
+                        <p x-show="!searchBox" class="ml-0">DealsDay</p>
+                        <input x-show="searchBox" type="text" class="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300" placeholder="Search">
                     </div>
                 </a>
             </div>
 
             <!-- Mobile menu button -->
             <div class="flex lg:hidden">
+                <button x-cloak @click="searchBox = !searchBox" class="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400 mr-2">
+                    <svg x-show="!searchBox" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                    </svg>
+                    <svg x-show="searchBox" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </button>
                 <button x-cloak @click="isOpen = !isOpen" type="button" class="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="toggle menu">
                     <svg x-show="!isOpen" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M4 8h16M4 16h16" />
@@ -30,7 +39,7 @@
                 <a href="#" class="px-2.5 py-2 text-gray-700 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 md:mx-2">Home</a>
             </div>
 
-            <div class="relative mt-4 md:mt-0">
+            <div class="relative mt-4 md:mt-0 hidden md:block">
                 <span class="absolute inset-y-0 left-0 flex items-center pl-3">
                     <svg class="w-5 h-5 text-gray-400" viewBox="0 0 24 24" fill="none">
                         <path d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
