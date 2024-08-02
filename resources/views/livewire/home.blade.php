1aa487b16ba997fdad801a5a6c3df333f24fe979
Rajesh-Dehury <56267680+Rajesh-Dehury@users.noreply.github.com>
2024-08-02 14:50:08 +0530

    ui changes

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,3 +1,60 @@
-<div>
-    {{-- If your happiness depends on money, you will never be happy with yourself. --}}
+<div class="p-5">
+    <div class="flex justify-center flex-col md:flex-row flex-wrap gap-2">
+        <div class="bg-white border p-3 w-full md:w-1/4 rounded-xl">
+            <div class="relative">
+                <p class="text-xs bg-gray-500 text-white rounded-tl-lg absolute top-0 left-0 px-2 py-1.5">July 30, 2024</p>
+                <img src="https://picsum.photos/200/300" alt="" class="h-40 w-full object-cover rounded-lg">
+            </div>
+            <div class="mt-3">
+                <p class="text-sm mb-1 text-gray-500">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos sequi, beatae architecto eligendi quibusdam</p>
+                <p class="font-bold text-green-600 hidden">Up to 90% off</p>
+            </div>
+            <div class="flex mt-3">
+                <a href="" class="bg-red-700 hover:bg-red-600 text-white w-1/2 text-center rounded-lg py-1.5 mx-1 font-bold">Details</a>
+                <a href="" class="bg-blue-500 hover:bg-blue-400 text-white w-1/2 text-center rounded-lg py-1.5 mx-1 font-bold">Check Now</a>
+            </div>
+        </div>
+        <div class="bg-white border p-3 w-full md:w-1/4 rounded-xl">
+            <div class="relative">
+                <p class="text-xs bg-gray-500 text-white rounded-tl-lg absolute top-0 left-0 px-2 py-1.5">July 30, 2024</p>
+                <img src="https://picsum.photos/200/300" alt="" class="h-40 w-full object-cover rounded-lg">
+            </div>
+            <div class="mt-3">
+                <p class="text-sm mb-1 text-gray-500">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos sequi, beatae architecto eligendi quibusdam</p>
+                <p class="font-bold text-green-600 hidden">Up to 90% off</p>
+            </div>
+            <div class="flex mt-3">
+                <a href="" class="bg-red-700 hover:bg-red-600 text-white w-1/2 text-center rounded-lg py-1.5 mx-1 font-bold">Details</a>
+                <a href="" class="bg-blue-500 hover:bg-blue-400 text-white w-1/2 text-center rounded-lg py-1.5 mx-1 font-bold">Check Now</a>
+            </div>
+        </div>
+        <div class="bg-white border p-3 w-full md:w-1/4 rounded-xl">
+            <div class="relative">
+                <p class="text-xs bg-gray-500 text-white rounded-tl-lg absolute top-0 left-0 px-2 py-1.5">July 30, 2024</p>
+                <img src="https://picsum.photos/200/300" alt="" class="h-40 w-full object-cover rounded-lg">
+            </div>
+            <div class="mt-3">
+                <p class="text-sm mb-1 text-gray-500">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos sequi, beatae architecto eligendi quibusdam</p>
+                <p class="font-bold text-green-600 hidden">Up to 90% off</p>
+            </div>
+            <div class="flex mt-3">
+                <a href="" class="bg-red-700 hover:bg-red-600 text-white w-1/2 text-center rounded-lg py-1.5 mx-1 font-bold">Details</a>
+                <a href="" class="bg-blue-500 hover:bg-blue-400 text-white w-1/2 text-center rounded-lg py-1.5 mx-1 font-bold">Check Now</a>
+            </div>
+        </div>
+        <div class="bg-white border p-3 w-full md:w-1/4 rounded-xl">
+            <div class="relative">
+                <p class="text-xs bg-gray-500 text-white rounded-tl-lg absolute top-0 left-0 px-2 py-1.5">July 30, 2024</p>
+                <img src="https://picsum.photos/200/300" alt="" class="h-40 w-full object-cover rounded-lg">
+            </div>
+            <div class="mt-3">
+                <p class="text-sm mb-1 text-gray-500">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos sequi, beatae architecto eligendi quibusdam</p>
+                <p class="font-bold text-green-600 hidden">Up to 90% off</p>
+            </div>
+            <div class="flex mt-3">
+                <a href="" class="bg-red-700 hover:bg-red-600 text-white w-1/2 text-center rounded-lg py-1.5 mx-1 font-bold">Details</a>
+                <a href="" class="bg-blue-500 hover:bg-blue-400 text-white w-1/2 text-center rounded-lg py-1.5 mx-1 font-bold">Check Now</a>
+            </div>
+        </div>
+    </div>
 </div>
