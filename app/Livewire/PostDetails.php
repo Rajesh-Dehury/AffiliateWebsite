47f6a20f3192a60102883ba4c9e76dc5a95a53fa
Rajesh-Dehury <56267680+Rajesh-Dehury@users.noreply.github.com>
2024-08-12 08:52:36 +0530

    details page more product show

--- app/Livewire/PostDetails.php
+++ app/Livewire/PostDetails.php
@@ -9,17 +9,20 @@ class PostDetails extends Component
 {
     public $prod_id;
     public $record;
+    public $record_latests;
 
     public function mount($prod_id)
     {
         $this->prod_id = $prod_id;
         $this->record = AmazonDeals::find($prod_id);
+        $this->record_latests = AmazonDeals::latest()->where('id', '!=', $this->prod_id)->take(3)->get();
     }
 
     public function render()
     {
         return view('livewire.post-details', [
-            'record' => $this->record
+            'record' => $this->record,
+            'record_latests' => $this->record_latests,
         ])->layout('components.home-layout');
     }
 }
--- resources/views/livewire/post-details.blade.php
+++ resources/views/livewire/post-details.blade.php
@@ -55,6 +55,35 @@
                 <p class="text-sm mt-3 font-mono text-yellow-950">Product prices and availability are accurate as of the date/time indicated and are subject to change. Any price and availability information displayed on Amazon India at the time of purchase will apply to the purchase of this product.</p>
             </div>
         </div>
-        <div class="col-span-12 md:col-span-4"></div>
+        <div class="col-span-12 md:col-span-4">
+            <div class="flex justify-between">
+                <p class="text-2xl mb-4 mt-4 md:mt-0">Other offers</p>
+            </div>
+            @forelse($record_latests as $record)
+            <div class="bg-white border p-1 col-span-12 md:col-span-4 rounded-xl mb-3">
+                <div class="relative">
+                    <p class="text-xs bg-gray-700 text-white rounded-tl-lg absolute top-0 left-0 px-2 py-1.5">{{$record->updated_at->format('F j, Y')}}</p>
+                    @if($record->saving_percent)
+                    <p class="text-xs bg-red-700 text-white rounded-tr-lg absolute top-0 right-0 px-2 py-1.5">UPTO {{$record->saving_percent}} % OFF</p>
+                    @endif
+                    <img src="{{$record->primary_large_url}}" alt="" class="h-60 w-full rounded-lg object-contain">
+                    <div class="h-24 w-full bg-gradient-to-t from-gray-800 bottom-0 rounded-lg">
+                        <div class="absolute bottom-2 px-3">
+                            <p class="text-sm mb-1 text-white">{{\Str::limit($record->product_title,70)}}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-3">
+                    <p class="font-bold text-green-600 hidden">Up to 90% off</p>
+                </div>
+                <div class="flex my-2">
+                    <a wire:navigate href="{{route('details',$record->id)}}" class="bg-green-700 hover:bg-green-600 text-white w-1/2 text-center rounded-lg py-1.5 mx-1 font-bold">Details</a>
+                    <a href="{{$record->our_link}}" target="_blank" class="bg-blue-500 hover:bg-blue-400 text-white w-1/2 text-center rounded-lg py-1.5 mx-1 font-bold">Check Now</a>
+                </div>
+            </div>
+            @empty
+            <p>No Posts Found</p>
+            @endforelse
+        </div>
     </div>
 </div>
